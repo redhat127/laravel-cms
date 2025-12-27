33a56b29c362331a9a7518e8f28d0b11f35e6659
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-27 10:12:18 +0330

    init

---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,13 @@
+export type UsersTable = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  email_verified_at: string | null;
+  created_at: string | null;
+  updated_at: string | null;
+};
+
+export type CurrentUser = {
+  currentUser: Pick<UsersTable, 'id' | 'name' | 'username' | 'email' | 'email_verified_at' | 'created_at' | 'updated_at'> | null;
+};
