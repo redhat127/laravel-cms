8eccee1fe5a4bfbe2ee411b21633a2819b7f2bb6
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-12-29 10:39:39 +0330

    fix: improve email change notification clarity
    - Fix capitalization in notification message
    - Clarify that verification email is sent to new address
    - Emphasize verification requirement before login
    - Improve overall message readability

--- resources/views/account/your-email-changed.blade.php
+++ resources/views/account/your-email-changed.blade.php
@@ -3,15 +3,16 @@
 
 Dear {{ $name }},
 
-Your email has been successfully changed from **{{ $old_email }}** to **{{ $new_email }}**. you need to verify your new email to login.
+Your email has been successfully changed from **{{ $old_email }}** to **{{ $new_email }}**. A verification email has been sent to your new email address.
 
 **Changed on:**
 - UTC: {{ $email_changed_at_utc }}
 - Tehran: {{ $email_changed_at_tehran }}
+
+**Important:** You will need to verify your new email address before you can login again.
 
 If you did not make this change, please contact our support team immediately to secure your account.
 
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
-
